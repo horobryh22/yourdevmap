9fb476228ea5553ceebb5483b1b90feec8c28ac9
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-11-25 19:44:12 +0500

    chor: add h1 title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { CoursesList } from "@/features/CoursesList/pub/CoursesList";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
+      <h1>Courses</h1>
       <CreateCourseForm revalidatePagePath="/" className="w-[300px]" />
       <CoursesList revalidatePagePath="/" />
     </main>
