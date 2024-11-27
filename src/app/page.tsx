0501f739914ae62412897a65e03dd88dfbaa78c1
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-11-27 17:58:14 +0500

    ui: change h1 text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CoursesList } from "@/features/CoursesList/pub/CoursesList";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1>courses list</h1>
+      <h1>COURSE LIST</h1>
       <CreateCourseForm revalidatePagePath="/" className="w-[300px]" />
       <CoursesList revalidatePagePath="/" />
     </main>
