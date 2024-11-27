8551b49bde2b28f939570652b96cb1bc6dd50c21
Ilya Khorobrykh <i.v.horobryh@gmail.com>
2024-11-27 17:30:51 +0500

    chor: change ui for test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { CoursesList } from "@/features/CoursesList/pub/CoursesList";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1>Courses List Test</h1>
+      <h1>Courses List</h1>
       <CreateCourseForm revalidatePagePath="/" className="w-[300px]" />
       <CoursesList revalidatePagePath="/" />
     </main>
